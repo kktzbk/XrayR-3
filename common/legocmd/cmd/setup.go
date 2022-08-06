40fc2661eafdbbb09fbbc4dcd783a4b4cc5e55b4
kktzbk <99929123+kktzbk@users.noreply.github.com>
2022-08-06 16:38:52 +0800

    Update setup.go

--- common/legocmd/cmd/setup.go
+++ common/legocmd/cmd/setup.go
@@ -9,7 +9,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/ilovecpc/XrayR/common/legocmd/log"
+	"github.com/kktzbk/XrayR-3/common/legocmd/log"
 	"github.com/go-acme/lego/v4/certcrypto"
 	"github.com/go-acme/lego/v4/lego"
 	"github.com/go-acme/lego/v4/registration"
